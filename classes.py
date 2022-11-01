8256c9be73c7008e00b24e0461cb4de3834a9412
Rafael Felipe dos Santos <99187467+rafael1509@users.noreply.github.com>
2022-11-01 14:00:12 -0300

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,18 @@
+class enemyCar(pygame.sprite.Sprite):
+  pass
+
+class itens(pygame.sprite.Sprite):
+  '''
+  pegar mais vidas ou itens no mapa
+  '''
+  pass
+
+class player_car(pygame.sprite.Sprite):
+  pass
+
+
+class game_backgroud(pygame.sprite.Sprite):
+  pass
+
+class button(pygame.sprite.Sprite):
+  pass
